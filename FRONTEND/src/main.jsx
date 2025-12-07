5da8374d4b8e559f1940667e6e94f86094922ae4
crashelly <buitragoagudeloj006@gmail.com>
2025-12-06 20:52:25 -0500

    se cambio el nombre del archivo impoortado /App/store a /App/Store

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import './assets/tienda/style.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { Store } from "./app/store"
+import { Store } from "./App/Store"
 
 
 createRoot(document.getElementById('root')).render(
